95d0857602f34065777d6bfb60c6449fe8f8897b
Ribamar Neto <ribaferreiraneto@gmail.com>
2024-08-16 22:54:44 -0300

    Otimmizando um pouco o slider

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,6 +1,9 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Header from '../components/Header'
 import { useNavigate } from "react-router-dom";
+import '/node_modules/swiper/swiper.min.css';
+import './home.css'
+
 
 function Home() {
   const navigate = useNavigate();
@@ -9,22 +12,23 @@ function Home() {
     <>
     <title>Home</title>
     <Header/>
-    <div style={{ padding: '20px' }}></div>
+    <div ></div>
         <Swiper
-            spaceBetween={50}
+            spaceBetween={10}
             slidesPerView={1}
             pagination={{ clickable: true }}
             navigation
             autoplay={{ delay: 2500, disableOnInteraction: false }}
-            style={{ width: '100%', height: '400px' }}
+            style={{ width: '100%', height: '100%' }}
+            className='swiper'
         >
-            <SwiperSlide>
+            <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}} className='swiper'>
             <img src="https://via.placeholder.com/800x400?text=Slide+1" alt="Slide 1" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className='swiper'>
             <img src="https://via.placeholder.com/800x400?text=Slide+2" alt="Slide 2" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className='swiper'>
             <img src="https://via.placeholder.com/800x400?text=Slide+3" alt="Slide 3" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </SwiperSlide>
         </Swiper>
